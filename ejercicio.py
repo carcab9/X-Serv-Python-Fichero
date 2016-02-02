8e2cd707ce14debe4cc1a5ab9aa06f1663b62523
Gregorio Robles <grex@gsyc.urjc.es>
2016-02-02 12:46:56 +0100

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fd = open('/etc/passwd', 'r')
+
+lineas = fd.readlines()
+fd.close()
+
+for linea in lineas:
+    elementos = linea.split(':')
+    print elementos[0], elementos[-1][:-1]
+
+print "Hay", len(lineas), "usuarios"
+
 
